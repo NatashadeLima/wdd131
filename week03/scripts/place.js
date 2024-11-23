1a80ac6869b23906f5e3fea9b5f97f988bc782d0
NatashadeLima <lim22010@byui.edu>
2024-11-23 00:54:05 -0300

    Place

---
+++ week03/scripts/place.js
@@ -0,0 +1,5 @@
+
+const lastModifiedDate = new Date(document.lastModified);
+const formattedDate = lastModifiedDate.toLocaleString();
+document.getElementById("lastModified").textContent += formattedDate;
+
